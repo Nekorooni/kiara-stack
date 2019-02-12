ac225ad8e47a93e4e2a9b8e4c7176bcfd9cd8d10
Rooni <nekorooni@gmail.com>
2019-02-12 11:48:02 +0100

    Further improve dev startup files for windows
    Will now automatically rename the override file, and added some comments.

--- dev start.bat
+++ dev start.bat
@@ -1,2 +1,6 @@
+@echo off
+rem Rename dev docker-compose file if it's not
+if exist docker-compose-dev.yml ren docker-compose-dev.yml docker-compose.override.yml
+rem Start up vagrant and ssh into it, cd to the right folder, and restart the kiara service
 vagrant up
 vagrant ssh -- -t 'cd /usr/src/kiara-stack/; docker-compose up -d; sleep 5s; docker-compose restart kiara; /bin/bash'
